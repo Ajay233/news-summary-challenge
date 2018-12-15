d244ded3f83599564564da2d10a3c0e2ee17fd09
Ajay <ajaymungur@hotmail.com>
2018-12-15 00:54:56 +0000

    Set up testing framework

---
+++ test_framework.js
@@ -0,0 +1,35 @@
+function describe(description, func){
+  console.log(description);
+  func();
+};
+
+var it = function(message, func){
+  describe(' ' + message, func)
+};
+
+function expect(exp){
+  return matchers(exp);
+};
+
+var matchers = function matchers(exp){
+  return {
+    toEqual: function toEqual(assertion){
+      if (exp === assertion) {
+        console.log("pass");
+        return true;
+      } else {
+        console.log("fail");
+        return false;
+      }
+    },
+    toBeAnArray: function toBeAnArray(assertion){
+      if (Array.isArray(exp) === assertion) {
+        console.log("pass");
+        return true;
+      } else {
+        console.log("fail");
+        return false;
+      }
+    }
+  };
+};
